71c22574b85bdbbff56ce67d5b36184080bc77e3
vulh1209 <vulh@ttgvn.com>
2021-05-25 07:16:10 +0700

    sync file

---
+++ src/cpns/SubMenu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../cpns/Menu.css';
+
+export default function SubMenu() {
+    return (
+        <div className="menu submenu">
+           <ul>
+               <li><a href='#'>DOGE FARMING</a></li>
+               <li><a href='#'>DOGE SOCIALIZING</a></li>
+               <li><a href='#'>DOGE FIGHTING</a></li>
+               <li><a href='#'>DOGE EVOLVING</a></li>
+           </ul>
+        </div>
+    )
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,52 +1,83 @@
-import React from 'react'
-import Menu from '../cpns/Menu'
-import '../screens/Home.css';
-import Line from '../imgs/Line.svg';
-import Avatar from '../imgs/Avatar.svg';
-import MainStory from '../imgs/MainStory.svg';
-import Card from '../cpns/Card'
+import React, { useState } from 'react';
+import Menu from "../cpns/Menu";
+import SubMenu from "../cpns/SubMenu";
+import "../screens/Home.css";
+import Line from "../imgs/Line.svg";
+import Avatar from "../imgs/Avatar.svg";
+import MainStory from "../imgs/MainStory.svg";
+import Card from "../cpns/Card";
+
+import Pic1 from "../imgs/Pic1.svg";
+import Pic2 from "../imgs/Pic2.svg";
+// import Pic3 from "../imgs/Pic3.svg";
 
 export default function Home() {
-    return (
-        <div id='home'>
-            <div id='banner'>
-                <Menu />
-                <div id='banner-div'>
-                    <div id='banner-p'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </div>
-                    <a id='banner-a' href='#'>
-                        GET A SYPHA
-                    </a>
-                </div>
-            </div>
-            <div id='mainstory'>
-                <div>
-                    <div>
-                        <p className='titlefont'>THE MAIN STORY</p>
-                        <img src={Line} alt='Line' />
-                    </div>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                </div>
-                <img src={MainStory} alt='MainStory' />
-
-            </div>
-            <div id='dogeagent'>
-                <div id='dogeagent-title'>
-                    <p className='titlefont'>DOGE AGENTS</p>
-                    <img src={Line} alt='Line' />
-                </div>
-                <div id='dogeagent-cards'>
-                    <Card link={Avatar} />
-                    <Card link={Avatar} />
-                    <Card link={Avatar} />
-                    <Card link={Avatar} />
-                    <Card link={Avatar} />
-                </div>
-            </div>
-            <div id='gameplay'></div>
+    const [PicPick, setPicPick] = useState(Pic1)
+    const [PicList, setPicList] = useState([Pic1,Pic2])
+  return (
+    <div id="home">
+      <div id="banner">
+        <Menu />
+        <div id="banner-div">
+          <div id="banner-p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+          <a id="banner-a" href="#">
+            GET A SYPHA
+          </a>
         </div>
-    )
+      </div>
+      <div id="mainstory">
+        <div>
+          <div>
+            <p className="titlefont">THE MAIN STORY</p>
+            <img src={Line} alt="Line" />
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.
+          </p>
+        </div>
+        <img src={MainStory} alt="MainStory" />
+      </div>
+      <div id="dogeagent">
+        <div id="dogeagent-title">
+          <p className="titlefont">DOGE AGENTS</p>
+          <img src={Line} alt="Line" />
+        </div>
+        <div id="dogeagent-cards">
+          <Card link={Avatar} />
+          <Card link={Avatar} />
+          <Card link={Avatar} />
+          <Card link={Avatar} />
+          <Card link={Avatar} />
+        </div>
+      </div>
+      <div id="gameplay">
+        <div id="gameplay-title">
+          <p className="titlefont">GAMEPLAY</p>
+          <img src={Line} alt="Line" />
+        </div>
+        <div id="gameplay-menu">
+          <SubMenu />
+        </div>
+        <div id="gameplay-gallery">
+          <img src={PicPick}/>
+          <div>
+            <div>
+              <img />
+              <img />
+            </div>
+            <div>
+              <img />
+              <img />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
